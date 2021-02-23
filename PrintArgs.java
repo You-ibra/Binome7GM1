037faaca278fca57df88563b18f769f5e5582fea
You-ibra <79516652+You-ibra@users.noreply.github.com>
2021-02-23 15:10:12 +0100

    Add files via upload

---
+++ PrintArgs.java
@@ -0,0 +1,13 @@
+
+
+	public class PrintArgs {
+		public static void main(String[] Args) {
+		  	int i;
+		  	for (i = 0; i < Args.length; i++)   
+		  		{
+		  		System.out.println(Args[i]);
+		  		}
+		  	
+		  System.out.println(Args.length);
+		  	}
+		  }
