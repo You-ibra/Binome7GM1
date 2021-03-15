973ad3efa104d49acdedeb6544baa666820f5db7
You-ibra <79516652+You-ibra@users.noreply.github.com>
2021-03-15 23:41:00 +0100

    Add files via upload

---
+++ tp1/Vector_TP1.java
@@ -0,0 +1,46 @@
+
+public class Vector {
+	
+	private Rational [] tab = new Rational[2];
+	
+	public Vector(Rational x, Rational y)
+	{
+	tab[0]=x;
+	tab[1]=y;
+	}
+	
+	public String toString() 
+	{
+		return ("("+ tab[0].num +"/"+tab[0].denom+"," + tab[1].num +"/"+ tab[1].denom+ ")");
+		
+	}
+	
+	public void set(int i, Rational r) {
+		if (this.tab.length<i) {
+			 throw new ArithmeticException("Erreur, le denominateur ne peut pas être nul" ); 
+		}
+		else {
+			this.tab[i]=r;
+			
+		}
+		
+	}
+	
+	public Rational get(int i) {
+		if (this.tab.length<i) {
+			 throw new ArithmeticException("Erreur, le denominateur ne peut pas être nul" ); 
+		}
+		else {
+			return (tab[i]);
+		
+		
+			}
+	
+	}
+	public void multRationnal(Rational r1) {
+		this.tab[0].mult(r1);
+		this.tab[1].mult(r1);
+	}
+	
+	
+}
